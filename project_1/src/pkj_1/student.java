3c22a5d27fd07e958f4259b6bd9d7546531ab62e
SAHIL <SAHIL@LAPTOP-COCEV5P7>
2023-01-30 15:11:30 +0530

    first time pushing

---
+++ project_1/src/pkj_1/student.java
@@ -0,0 +1,23 @@
+package pkj_1;
+
+public class student 
+{
+  int age;
+  int rollno;
+  public void ab()
+  {
+    System.out.println("welcome to all of you"); 
+    System.out.println("automation is very easy");
+  }
+public static void main(String[] args) 
+{
+student abc= new student();	
+abc.ab();
+abc.age=18;
+abc.rollno=1;
+	System.out.println(abc.age);
+	System.out.println(abc.rollno);
+	
+}
+
+}
